50d29ad8cb7fea9abf3b3f77ad150823219b7e14
chrysomallos <68998811+chrysomallos@users.noreply.github.com>
2024-05-19 14:15:05 +0200

    test: fix issues in test definitions

--- test/card.spec.mjs
+++ test/card.spec.mjs
@@ -3,51 +3,57 @@ import Card from '../src/card.mjs';
 import Faction from '../src/faction.mjs';
 import Factions from '../src/factions.mjs';
 
-describe('[Card] class tests', () => {
-  describe('constructor tests', () => {
-    it('successful', () => {
+describe('[Card] class tests', function() {
+  describe('constructor tests', function() {
+    it('successful', function() {
       const card = new Card(1, new Faction(1, 1, 'DE'), 99);
       const {code, count} = card.codeAndCount;
       assert.equal(code, '01DE099');
       assert.equal(count, 1);
     });
-    it('invalid set', () => {
+
+    it('invalid set', function() {
       assert.throws(() => new Card(0, new Faction(1, 1, 'DE'), 1));
     });
-    it('invalid faction', () => {
+
+    it('invalid faction', function() {
       assert.throws(() => new Card(1, 0, 1));
     });
-    it('invalid id', () => {
+
+    it('invalid id', function() {
       assert.throws(() => new Card(1, new Faction(1, 1, 'DE'), 1111));
     });
   });
 
-  describe('card getter', () => {
+  describe('card getter', function() {
     const card = new Card(1, new Faction(1, 1, 'DE'), 99, 2);
-    it('code', () => {
+
+    it('code', function() {
       assert.equal(card.code, '01DE099');
     });
-    it('faction code', () => {
+
+    it('faction code', function() {
       assert.equal(card.factionCode, 'DE');
     });
-    it('faction version', () => {
+
+    it('faction version', function() {
       assert.equal(card.factionVersion, 1);
     });
   });
 
-  describe('fromCode tests', () => {
-    it('invalid code throws exception', () => {
+  describe('fromCode tests', function() {
+    it('invalid code throws exception', function() {
       assert.throws(() => Card.fromCode('01PZ'));
       assert.throws(() => Card.fromCode('01PZ001:'));
       assert.throws(() => Card.fromCode('01PZ01:1'));
       assert.throws(() => Card.fromCode('01PZ0001'));
     });
 
-    it('invalid faction throws exception', () => {
+    it('invalid faction throws exception', function() {
       assert.throws(() => Card.fromCode('01AA001'));
     });
 
-    it('initialize from code default count', () => {
+    it('initialize from code default count', function() {
       const card = Card.fromCode('01PZ019');
       assert.equal(card.set, 1);
       assert.equal(card.faction.code, 'PZ');
@@ -55,7 +61,7 @@ describe('[Card] class tests', () => {
       assert.equal(card.count, 1);
     });
 
-    it('initialize from code with count', () => {
+    it('initialize from code with count', function() {
       const card = Card.fromCode('01PZ001:3');
       assert.equal(card.set, 1);
       assert.equal(card.faction.code, 'PZ');
@@ -63,7 +69,7 @@ describe('[Card] class tests', () => {
       assert.equal(card.count, 3);
     });
 
-    it('initialize from code and count', () => {
+    it('initialize from code and count', function() {
       const card = Card.fromCodeAndCount({code: '01PZ001', count: 3});
       assert.equal(card.set, 1);
       assert.equal(card.faction.code, 'PZ');
@@ -72,84 +78,98 @@ describe('[Card] class tests', () => {
     });
   });
 
-  describe('compare method must return correct value', () => {
-    it('(<0) sort a before b', () => {
+  describe('compare method must return correct value', function() {
+    it('(<0) sort a before b', function() {
       assert.equal(Card.compare(Card.fromCode('01DE001'), Card.fromCode('01DE002')) < 0, true);
     });
 
-    it('(=0) keep original order of a and b', () => {
+    it('(=0) keep original order of a and b', function() {
       assert.equal(Card.compare(Card.fromCode('02DE002'), Card.fromCode('02DE002')) === 0, true);
     });
 
-    it('(>0) sort b before a', () => {
+    it('(>0) sort b before a', function() {
       assert.equal(Card.compare(Card.fromCode('02BC002'), Card.fromCode('02DE002')) > 0, true);
     });
   });
 
-  describe('equals method must return correct value', () => {
-    it('same card', () => {
+  describe('equals method must return correct value', function() {
+    it('same card', function() {
       assert.equal(Card.fromCode('01DE001').equals(Card.fromCode('01DE001')), true);
     });
-    it('not equal id', () => {
+
+    it('not equal id', function() {
       assert.equal(Card.fromCode('01DE001').equals(Card.fromCode('01DE002')), false);
     });
-    it('not equal set', () => {
+
+    it('not equal set', function() {
       assert.equal(Card.fromCode('01DE001').equals(Card.fromCode('02DE001')), false);
     });
-    it('not equal faction', () => {
+
+    it('not equal faction', function() {
       assert.equal(Card.fromCode('01DE001').equals(Card.fromCode('01BW001')), false);
     });
   });
 
-  describe('static equals method must return correct value', () => {
-    it('same card', () => {
+  describe('static equals method must return correct value', function() {
+    it('same card', function() {
       assert.equal(Card.equals(Card.fromCode('01DE001'), Card.fromCode('01DE001')), true);
     });
-    it('not equal id', () => {
+
+    it('not equal id', function() {
       assert.equal(Card.equals(Card.fromCode('01DE001'), Card.fromCode('01DE002')), false);
     });
   });
 
-  describe('codeAndCount method must return correct values', () => {
-    it('set, faction, id and count check', () => {
+  describe('codeAndCount method must return correct values', function() {
+    it('set, faction, id and count check', function() {
       const {code, count} = new Card(1, Factions.fromCode('DE'), 1, 2).codeAndCount;
       assert.equal(code, '01DE001');
       assert.equal(count, 2);
     });
   });
 
-  describe('must detect correct faction from code', () => {
-    it('Demacia', () => {
+  describe('must detect correct faction from code', function() {
+    it('Demacia', function() {
       assert.equal(Card.fromCode('01DE001').factionId, 0);
     });
-    it('Freljord', () => {
+
+    it('Freljord', function() {
       assert.equal(Card.fromCode('01FR001').factionId, 1);
     });
-    it('Ionia', () => {
+
+    it('Ionia', function() {
       assert.equal(Card.fromCode('01IO001').factionId, 2);
     });
-    it('Noxus', () => {
+
+    it('Noxus', function() {
       assert.equal(Card.fromCode('01NX001').factionId, 3);
     });
-    it('Piltover & Zaun', () => {
+
+    it('Piltover & Zaun', function() {
       assert.equal(Card.fromCode('01PZ001').factionId, 4);
     });
-    it('Shadow Isles', () => {
+
+    it('Shadow Isles', function() {
       assert.equal(Card.fromCode('01SI001').factionId, 5);
     });
-    it('Bilgewater', () => {
+
+    it('Bilgewater', function() {
       assert.equal(Card.fromCode('01BW001').factionId, 6);
     });
-    it('Shurima', () => {
+
+    it('Shurima', function() {
       assert.equal(Card.fromCode('01SH001').factionId, 7);
     });
-    it('Mount Targon', () => {
+
+    it('Mount Targon', function() {
       assert.equal(Card.fromCode('01MT001').factionId, 9);
     });
-    it('Bandle City', () => {
+
+    it('Bandle City', function() {
       assert.equal(Card.fromCode('01BC001').factionId, 10);
     });
-    it('Runeterra', () => {
+
+    it('Runeterra', function() {
       assert.equal(Card.fromCode('01RU001').factionId, 12);
     });
   });
--- test/tools.spec.mjs
+++ test/tools.spec.mjs
@@ -3,76 +3,92 @@ import pad from '../utils/pad.mjs';
 import assert from 'assert';
 import VarInt from '../utils/var-int.mjs';
 
-describe('[Utils] full tests', () => {
-  describe('Base32', () => {
-    describe('validation', () => {
-      const plain = [1, 2, 3, 4, 5, 6, 7, 8, 9].flatMap((v) => VarInt.get(v));
-      it(`encode and decode test [${plain.join(',')}]`, () => {
+describe('[Utils] full tests', function () {
+  describe('Base32', function () {
+    describe('validation', function () {
+      let plain;
+
+      before(function () {
+        plain = [1, 20, 30, 400, 500, 600, 700, 800, 900].flatMap((v) => VarInt.get(v));
+      });
+
+      it(`encode and decode test`, function () {
         const encoded = Base32.encode(plain);
         const decoded = Base32.decode(encoded);
         assert.deepEqual(decoded, plain, 'decoded does not match plain');
       });
     });
 
-    describe('decode method', () => {
-      it('empty', () => {
+    describe('decode method', function () {
+      it('empty', function () {
         assert.deepEqual(Base32.decode(''), [0]);
       });
     });
 
-    describe('encode method', () => {
-      it('null value', () => {
+    describe('encode method', function () {
+      it('null value', function () {
         assert.equal(Base32.encode(null), '');
       });
 
-      it('empty value', () => {
+      it('empty value', function () {
         assert.equal(Base32.encode([]), '');
       });
     });
 
-    describe('speed test', () => {
-      const plain = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21].flatMap((v) => VarInt.get(v));
-      const encoded = Base32.encode(plain, true);
-      const decoded = Base32.decode(encoded);
-      it('encode with padding', () => {
+    describe('speed test', function () {
+      let plain, encoded, decoded;
+
+      before(function () {
+        plain = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21].flatMap((v) => VarInt.get(v));
+        encoded = Base32.encode(plain, true);
+        decoded = Base32.decode(encoded);
+      });
+
+      it('encode with padding', function () {
         for (let i = 0; i < 1000; i++) Base32.encode(plain, true);
       });
-      it('encode', () => {
+
+      it('encode', function () {
         for (let i = 0; i < 1000; i++) Base32.encode(plain);
       });
-      it('decode', () => {
+
+      it('decode', function () {
         for (let i = 0; i < 1000; i++) Base32.decode(encoded);
       });
-      it('validate', () => {
+
+      it('validate', function () {
         assert.deepEqual(decoded, plain, 'decoded does not match plain');
       });
     });
   });
 
-  describe('pad', () => {
-    it('smaller number', () => {
+  describe('pad', function () {
+    it('smaller number', function () {
       assert.equal(pad(99, 4), '0099');
     });
-    it('same', () => {
+
+    it('same', function () {
       assert.equal(pad(999, 3), '999');
     });
-    it('bigger number', () => {
+
+    it('bigger number', function () {
       assert.equal(pad(99999, 3), '99999');
     });
-    it('must fail if number is greater than size', () => {
+
+    it('must fail if number is greater than size', function () {
       assert.throws(() => pad(99, 1, true));
     });
   });
 
-  describe('VarInt', () => {
-    it ('get', () => {
+  describe('VarInt', function () {
+    it('get', function () {
       const arr = VarInt.get(180);
       assert.equal(arr.length, 2);
       assert.equal(arr[0], 180);
       assert.equal(arr[1], 1);
     });
 
-    it('pop', () => {
+    it('pop', function () {
       const arr = [180, 1];
       const num = VarInt.pop(arr);
       assert.equal(num, 180);
