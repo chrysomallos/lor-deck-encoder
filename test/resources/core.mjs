832e99c33024c69e4faf004564387a2b516f734f
chrysomallos <68998811+chrysomallos@users.noreply.github.com>
2024-05-20 20:35:30 +0200

    test: add tests for data dragon

---
+++ test/resources/core.mjs
@@ -0,0 +1,26 @@
+export default {
+  vocabTerms: [],
+  keywords: [],
+  regions: [{abbreviation: 'DE', iconAbsolutePath: 'http://dummy.local/core/en_us/img/regions/icon-demacia.png', name: 'Demacia', nameRef: 'Demacia'}],
+  spellSpeeds: [
+    {name: 'Slow', nameRef: 'Slow'},
+    {name: 'Burst', nameRef: 'Burst'},
+    {name: 'Fast', nameRef: 'Fast'},
+  ],
+  rarities: [
+    {name: 'COMMON', nameRef: 'Common'},
+    {name: 'RARE', nameRef: 'Rare'},
+    {name: 'EPIC', nameRef: 'Epic'},
+    {name: 'Champion', nameRef: 'Champion'},
+    {name: 'None', nameRef: 'None'},
+  ],
+  sets: [{iconAbsolutePath: 'http://dummy.local/core/en_us/img/sets/set1_crispmip.png', name: 'Foundations', nameRef: 'Set1'}],
+  formats: [],
+  adventureRarities: [
+    {name: 'COMMON', nameRef: 'Common'},
+    {name: 'RARE', nameRef: 'Rare'},
+    {name: 'EPIC', nameRef: 'Epic'},
+    {name: 'LEGENDARY', nameRef: 'Legendary'},
+    {name: 'SPECIAL', nameRef: 'Special'},
+  ],
+};
