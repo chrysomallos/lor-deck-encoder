e5170c8d05d955481adacc61d2ff113a6a720b93
chrysomallos <68998811+chrysomallos@users.noreply.github.com>
2024-08-25 14:40:33 +0200

    refactor: split performance tests

---
+++ performance/base32.perf.mjs
@@ -0,0 +1,30 @@
+import Base32 from '../utils/base32.mjs';
+import assert from 'assert';
+import VarInt from '../utils/var-int.mjs';
+
+describe('[Base32] performance test', function () {
+  let plain, encoded, decoded;
+  const performanceCalls = 100000;
+
+  before(function () {
+    plain = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21].flatMap(v => VarInt.get(v));
+    encoded = Base32.encode(plain, true);
+    decoded = Base32.decode(encoded);
+  });
+
+  it('encode, padding is true', function () {
+    for (let i = 0; i < performanceCalls; i++) Base32.encode(plain, true);
+  });
+
+  it('encode, padding is false', function () {
+    for (let i = 0; i < performanceCalls; i++) Base32.encode(plain);
+  });
+
+  it('decode', function () {
+    for (let i = 0; i < performanceCalls; i++) Base32.decode(encoded);
+  });
+
+  it('validate', function () {
+    assert.deepEqual(decoded, plain, 'decoded does not match plain');
+  });
+});
