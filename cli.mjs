dbb11755a3feb69892c93b5984e669e1a76456e4
chrysomallos <68998811+chrysomallos@users.noreply.github.com>
2024-11-10 16:09:07 +0100

    fix: cli code and readme

--- cli.mjs
+++ cli.mjs
@@ -5,10 +5,20 @@ import stringify from 'json-stringify-pretty-compact';
 import {Deck, generateDataDragon} from './index.mjs';
 import Base32 from './utils/base32.mjs';
 
-const [code, ...parameters] = process.argv.slice(2);
+const parameters = Object.fromEntries(Object.entries(minimist(process.argv.slice(2))).map(([k, v]) => [camelCase(k), v]));
+let [code] = parameters[''];
+code ??= parameters.code;
 
-if (parameters?.length) {
-  const {style, language, outFile, verify} = Object.fromEntries(Object.entries(minimist(parameters)).map(([k, v]) => [camelCase(k), v]));
+if (!code || parameters.help) {
+  console.log('Start by `yarn cli [--code] <code> ...`:');
+  console.log(' [--code <code>] : The code example `yarn cli CEAAECABAIDASDASDISC2OIIAECBGGY4FAWTINZ3AICACAQXDUPCWBABAQGSOKRM`');
+  console.log(' [--help]');
+  console.log(' [--language <language>]');
+  console.log(' [--out-file <file path>]');
+  console.log(' [--style]');
+  console.log(' [--verify]');
+} else {
+  const {style, language, outFile, verify} = parameters;
   if (verify) {
     const deck = Deck.fromCode(code);
     console.log(JSON.stringify(deck.list));
@@ -16,7 +26,7 @@ if (parameters?.length) {
     console.log(`${code} => ${deck.code} (${code === deck.code})`);
     console.log(JSON.stringify(Deck.fromCode(deck.code).list));
     console.log(JSON.stringify(Base32.decode(deck.code)));
-  } else {
+  } else if (outFile?.length) {
     const dragon = generateDataDragon();
     const data =
       style === 'html' || outFile.endsWith('.html')
@@ -25,7 +35,7 @@ if (parameters?.length) {
 
     await fs.writeFile(outFile, data);
     console.log('written into', outFile);
+  } else {
+    console.log(Deck.fromCode(code).list);
   }
-} else {
-  console.log(Deck.fromCode(code).list);
 }
